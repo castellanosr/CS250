aad197333f3adf34bcf5ba1123e01d106774d812
castellanosr <31486151+castellanosr@users.noreply.github.com>
2018-01-24 21:13:35 -0500

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Lab1 {
+
+  public static void main(String[] args) {
+
+      Random rand = new Random();
+      int[] aray = new int[1000000];
+      
+      for(i=0; i < aray.length; i++ ) {
+        aray
+      }
+
+    }
+}
